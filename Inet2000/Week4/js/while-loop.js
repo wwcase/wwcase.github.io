3b3b964b59d031c892674df153fb183a9a7074c3
wwcase <wanda@wwcase.com>
2017-02-14 18:01:48 -0700

    Week4 Assignments

---
+++ Inet2000/Week4/js/while-loop.js
@@ -0,0 +1,1 @@
+var i = 1;       // Set counter to 1var msg = '';    // Message// Store 5 times table in a variablewhile (i < 40) {	if(i % 3 == 0 )   msg += i + ' x 7 = ' + (i * 7) + '<br />';  i++;	}document.getElementById('answer').innerHTML = msg;
